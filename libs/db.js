f22c0e60b1b8986664ff40579e9d8aa23acee63d
Moiz0 <bmoiz977@gmail.com>
2025-08-04 14:41:25 +0500

    first

--- libs/db.js
+++ libs/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB.");
-  } catch (error) {
-    console.log(error);
-  }
-};
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error("MONGODB_URI not defined");
 
-export default connectMongoDB;
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+export default async function connectMongoDB() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri).then(m => m);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
